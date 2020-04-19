09de8b0e84f8522666263e0954071dbed6d0b330
Shawpoo <994227083@qq.com>
2020-04-19 22:19:28 +0800

    [feature] 增加文字的点击事件

--- app/src/main/java/com/shawpoo/testdemo/MainActivity.kt
+++ app/src/main/java/com/shawpoo/testdemo/MainActivity.kt
@@ -2,11 +2,19 @@ package com.shawpoo.testdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_text.setOnClickListener {
+
+            Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
